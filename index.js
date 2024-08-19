676889e5ba01c5ee671cf4268588cba97ed62e9e
Avleen Sandhu <avleens28@gmail.com>
2024-08-19 05:23:56 -0700

    add fragrance form visibility

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JS is connected and logged')
+
+    let addFragrance = false;
+
+    const addBtn = document.getElementById("add-to-collection-btn")
+    const submitFormContainer = document.getElementById("submit-form-container")
+
+    addBtn.addEventListener('click', () => {
+        addFragrance = !addFragrance
+        if (addFragrance) {
+            submitFormContainer.style.display = "block";
+        } else {
+            submitFormContainer.style.display = "none"
+        }
+    })
+
+    fetchFragrances();
+
+    const fragranceSubmitForm = document.querySelector(".fragrance-submit-form")
+    fragranceSubmitForm.addEventListener('submit', handleFormSubmit)
 })
+
+function fetchFragrances(){
+    
+}
+
+function handleFormSubmit(){
+
+}
+
