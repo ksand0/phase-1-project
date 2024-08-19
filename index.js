d7570ca335741f83f191b046a61ded1f4ee28eda
Avleen Sandhu <avleens28@gmail.com>
2024-08-19 10:52:58 -0700

    handleFormSubmit function added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+let addFragrance = false;
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JS is connected and logged')
-
-    let addFragrance = false;
 
     const addBtn = document.getElementById("add-to-collection-btn")
     const submitFormContainer = document.getElementById("submit-form-container")
 
     addBtn.addEventListener('click', () => {
-        addFragrance = !addFragrance
+        addFragrance = !addFragrance;
         if (addFragrance) {
             submitFormContainer.style.display = "block";
         } else {
@@ -23,17 +23,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function fetchFragrances(){
     fetch('http://localhost:3000/fragrances')
-    .then(res => res.json)
+    .then(res => res.json())
     .then(fragrances => {
-        fragrances.forEach(fragrance => renderFragrances(fragrance))
+        fragrances.forEach(fragrance => renderFragrance(fragrance))
     })
 }
 
-function renderFragrances(){
 
+function handleFormSubmit(event){
+    event.preventDefault()
+    const fragranceSubmitForm = event.target
+    const fragranceImg = fragranceSubmitForm.image.value
+    const fragranceName = fragranceSubmitForm.name.value
+    const fragranceBrand = fragranceSubmitForm.brand.value
+    const fragranceNotes = fragranceSubmitForm.notes.value
+
+    const newFragrance = {
+        image : fragranceImg,
+        name : fragranceName,
+        brand : fragranceBrand,
+        notes: fragranceNotes
+    }
+
+    fetch('http://localhost:3000/fragrances', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newFragrance)
+    })
+    .then(res => res.json())
+    .then(fragrance => {
+        renderFragrance(fragrance);
+        fragranceSubmitForm.reset();
+        submitFormContainer.style.display = "none";
+        addFragrance = false;
+    })
 }
 
-function handleFormSubmit(){
+function handleDelete(event){
 
 }
-
